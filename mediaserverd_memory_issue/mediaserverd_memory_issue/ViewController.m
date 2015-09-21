e0e25fa3ce0e365bf23a00f87919a9b143b7234e
Musa Hindi and Travis Meisenheimer <ionicmobile+musa.hindi+travis.meisenheimer@asynchrony.com>
2015-09-21 09:52:19 -0500

    Add initial project setup

--- mediaserverd_memory_issue/mediaserverd_memory_issue/ViewController.m
+++ mediaserverd_memory_issue/mediaserverd_memory_issue/ViewController.m
@@ -1,6 +1,12 @@
 #import "ViewController.h"
 
+#import <AVKit/AVKit.h>
+#import <AVFoundation/AVFoundation.h>
+
 @interface ViewController ()
+
+@property (nonatomic) AVPlayerViewController *avplayerViewController;
+@property (nonatomic) NSURL *m3u8PlaylistUrl;
 
 @end
 
@@ -14,6 +20,15 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"AVPlayerModal"]) {
+        self.avplayerViewController = segue.destinationViewController;
+//        AVPlayer *player = [[AVPlayer alloc] initWithURL:self.m3u8PlaylistUrl];
+//        self.avplayerViewController.player = player;
+        
+    }
 }
 
 @end
